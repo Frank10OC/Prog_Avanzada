d4292d2e6f5685c8adc4d59d1b8a4f3a723970b2
Jhorchql <117998365+Jhorchql@users.noreply.github.com>
2022-11-28 20:22:50 -0500

    Update data.py

--- data.py
+++ data.py
@@ -13,10 +13,13 @@ st.markdown("""
 st.markdown("""
 ---
 La información contenida en esta página web permite acceder al Dataset “Índices Soberanos 2010 - 2022” 
-elaborado por el Ministerio de Economía y Finanzas del Perú. 
+elaborado por el Ministerio de Economía y Finanzas del Perú.
+
 Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)
+
 ---
 """)
+
 def load_data():
     url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
     return pd.read_csv(url)
