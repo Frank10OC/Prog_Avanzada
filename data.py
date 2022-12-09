6be34d187d1a23034bf4db21cd074268f6281a3b
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-12-09 12:35:45 -0500

    Update data.py

--- data.py
+++ data.py
@@ -204,11 +204,7 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:20px;">{"Gráfica: Renta Anual 
 st.line_chart(c, x='FECHA', y=["RENTA ANUAL ÍNDICE NOMINAL","RENTA ANUAL ÍNDICE REAL"])
 
 
-
-st.markdown(f'<h1 style="color:#fafdfa;font-size:30px;">{""}</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="color:#fafdfa;font-size:30px;">{""}</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="color:#fafdfa;font-size:30px;">{""}</h1>', unsafe_allow_html=True)
-st.markdown("\n")
+st.markdown("\n\n\n\n\n")
 col1, col2 = st.columns(2)
 with col1:
    st.image("https://facien.cayetano.edu.pe/images/documentacion/noticias/logos/logotipo_Ciencias.png", width=200)
