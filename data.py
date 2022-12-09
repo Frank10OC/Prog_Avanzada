5f2990349ac883e827dd7a4ebeb0b2fe63b7c0c8
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-12-08 23:35:15 -0500

    Update data.py

--- data.py
+++ data.py
@@ -2,13 +2,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
-
 #color de pagina
 st.markdown(
          f"""
