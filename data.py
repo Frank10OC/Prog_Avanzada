dc563308c73bdf73b9b272e63f9311700bb2d28e
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-12-09 03:44:16 -0500

    Update data.py

--- data.py
+++ data.py
@@ -142,10 +142,11 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Valor del Índice"}</h
 st.image("pond.jpg", width=200)
 st.latex(r'''
     W_{i;(f)} =
-    \frac{Q_{i;(f)} . P_{i;(f)}}{Q_{i;(f)} . P_{i;(f)} \lbrack Q_{i;(f)} . P_{i;(f)} \rbrack}
+    \frac{Q_{i;(f)} . P_{i;(f)}}{\textstyle\sum_{i}^n \lbrack Q_{i;(f)} . P_{i;(f)} \rbrack}
     ''')
 st.latex(r'''
     \sum_{\mathclap{i}} x_{n}
+    \textstyle\sum_{i=1}^n
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{i}^{n} =
     a \left(\frac{1-r^{n}}{1-r}\right)
