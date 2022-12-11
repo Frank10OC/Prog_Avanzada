89ee5371c4f5d831f141beb20413afa27cd30a4f
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-12-11 09:12:02 -0500

    Update data.py

--- data.py
+++ data.py
@@ -37,7 +37,7 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"- Parillo Sanchez, Yas
 
 st.markdown("---")
 st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"La información contenida en esta página web permite acceder al Dataset “Índices Soberanos 2010 - 2022” elaborado por el Ministerio de Economía y Finanzas del Perú (MEF)."""}</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Fuente de datos: [link](https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)"}</h1>', unsafe_allow_html=True)
 st.write("check out this [link](https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)")     
 #IMPORTANDO DATOS
 def load_data():
